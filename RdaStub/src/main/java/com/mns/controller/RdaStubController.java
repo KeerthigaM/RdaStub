8fd197a3c14b6bcb8a5ffc22f515b4de97ff287d
Keerthiga  M <886758@TCS.com>
2019-09-13 08:24:59 -0400

    RDA PT STUB Complete

--- RdaStub/src/main/java/com/mns/controller/RdaStubController.java
+++ RdaStub/src/main/java/com/mns/controller/RdaStubController.java
@@ -10,14 +10,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mns.domain.Category;
+import com.mns.domain.Equipment;
+import com.mns.domain.EquipmentDetails;
+import com.mns.domain.MuleWorkflowApproval;
+import com.mns.domain.MuleWorkflowRequest;
 import com.mns.domain.Product;
+import com.mns.domain.ProductWeights;
+import com.mns.domain.Report;
+import com.mns.domain.SiteGroup;
+import com.mns.domain.SupplyChain;
 import com.mns.domain.Upt;
+import com.mns.domain.Vendor;
+import com.mns.domain.Workflow;
+import com.mns.domain.message.WorkflowApprovalMessage;
+import com.mns.domain.message.WorkflowCreationMessage;
 import com.mns.service.impl.RdaStubServiceImpl;
 
 @RestController
@@ -28,30 +41,147 @@ public class RdaStubController {
 	@Resource
 	private RdaStubServiceImpl serviceImpl;
 	
-	@RequestMapping(value = "/product", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public String home(@RequestParam(value = "vendorNo", required = true) final String vendorNo, @RequestParam(value = "categories", required = true) final String categories){
-		return vendorNo+categories;
-	}
 	@RequestMapping(value = "/products", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Product>> search(@RequestParam(value = "vendorNo", required = true) final String vendorNo, @RequestParam(value = "categories", required = true) final String categories){
+	public ResponseEntity<List<Product>> getProducts(
+			@RequestParam(value = "vendorNo", required = true) final String vendorNo, 
+			@RequestParam(value = "categories", required = true) final String categories){
 		LOGGER.info("/products for vendor {} for categories {}", vendorNo, categories);
-		final List<Product> productList = serviceImpl.fetchProducts(vendorNo, categories);
-		LOGGER.info("/products for vendor {} for categories {} found {}", vendorNo, categories, productList.size());
+		final List<Product> productList = serviceImpl.getProducts(vendorNo, categories);
+		LOGGER.info("/products?vendor={}&categories={} found {}", vendorNo, categories, productList.size());
 		return new ResponseEntity<List<Product>>(productList, HttpStatus.OK);
-	}	
+	}
 	@RequestMapping(value = "/product/{articleNo}/vendor/{vendorNo}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Upt>> getCostPrice(@PathVariable("articleNo") final String articleNo, @PathVariable("vendorNo") final String vendorNo, @RequestParam(value = "orderUnit", required = false) final String
-			orderUnit, @RequestParam(value = "validFrom", required = true) final String validFrom){
+	public ResponseEntity<List<Upt>> getUpt(
+			@PathVariable("articleNo") final String articleNo, 
+			@PathVariable("vendorNo") final String vendorNo, 
+			@RequestParam(value = "orderUnit", required = false) final String orderUnit, 
+			@RequestParam(value = "validFrom", required = true) final String validFrom){
 		LOGGER.info("/product/{}/vendor/{}", articleNo, vendorNo);
 		final List<Upt> uptList = serviceImpl.getUpt(articleNo, vendorNo, orderUnit, validFrom);
 		LOGGER.info("/product/{}/vendor/{} found {}", articleNo, vendorNo, uptList.size());
 		return new ResponseEntity<List<Upt>>(uptList, HttpStatus.OK);
 	}	
 	@RequestMapping(value = "/categories", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Category>> getCategories(@RequestParam(value = "grp_category", required = true) final String grp_category){
+	public ResponseEntity<List<Category>> getCategories(
+			@RequestParam(value = "grp_category", required = true) final String grp_category){
 		LOGGER.info("/categories?grp_category={}", grp_category);
 		final List<Category> categoryList = serviceImpl.getCategories(grp_category);
 		LOGGER.info("/categories?grp_category={} found {}", grp_category, categoryList.size());
 		return new ResponseEntity<List<Category>>(categoryList, HttpStatus.OK);
+	}	
+	@RequestMapping(value = "/product/{articleNo}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Product>> getProduct(
+			@PathVariable("articleNo") final String articleNo, 
+			@RequestParam(value = "categories", required = true) final String categories){
+		LOGGER.info("AMLServiceImpl:getProduct for [articleNo: {}] and [categories : {}]", articleNo, categories);
+		final List<Product> productList = serviceImpl.getProduct(articleNo,categories);
+		LOGGER.info("/product/{}?categories={} found {}", articleNo,categories, productList.size());
+		return new ResponseEntity<List<Product>>(productList, HttpStatus.OK);
 	}
+	@RequestMapping(value = "/sitegroups", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<SiteGroup>> getSiteGroups(
+			@RequestParam(value = "classType", required = true) final String classType){
+		LOGGER.info("getSiteGroups for [classType: {}]", classType);
+		final List<SiteGroup> siteGroupList = serviceImpl.getSiteGroups(classType);
+		LOGGER.info("/sitegroups?classType={} found {}", classType, siteGroupList.size());
+		return new ResponseEntity<List<SiteGroup>>(siteGroupList, HttpStatus.OK);
+	}	
+	@RequestMapping(value = "/product/{articleNo}/sitegroup/{sitegroup}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<SupplyChain>> getSupplyChainData(
+			@PathVariable("articleNo") final String articleNo,
+			@PathVariable("sitegroup") final String sitegroup,
+			@RequestParam(value = "validFrom", required = true) final String validFrom,
+			@RequestParam(value = "classType", required = true) final String classType){
+		LOGGER.info("getSupplyChainData for [classType: {}][validFrom: {}]", classType, validFrom);
+		final List<SupplyChain> SupplyChainList = serviceImpl.getSupplyChainData(articleNo, sitegroup, classType, validFrom);
+		LOGGER.info("/product/{}/sitegroup/{}?classType={}&validFrom={} found {}", articleNo, sitegroup, classType, validFrom, SupplyChainList.size());
+		return new ResponseEntity<List<SupplyChain>>(SupplyChainList, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/workflow/trigger", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<WorkflowCreationMessage> triggerWorkflowData(
+			@RequestBody final MuleWorkflowRequest wfr){
+		LOGGER.info("triggerWorkflowData");
+		final WorkflowCreationMessage workflowCreationMessage = serviceImpl.triggerWorkflowData(wfr);
+		return new ResponseEntity<WorkflowCreationMessage>(workflowCreationMessage, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/workflows", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Workflow>> getPendingWorkflows(
+			@RequestParam(value = "categories", required = true) final String categories){
+		LOGGER.info("getPendingWorkflows for [categories: {}]", categories);
+		final List<Workflow> workflowList = serviceImpl.getPendingWorkflows(categories);
+		LOGGER.info("/workflows?categories={} found {}", categories, workflowList.size());
+		return new ResponseEntity<List<Workflow>>(workflowList, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/workflow/approve", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<WorkflowApprovalMessage> approveWorkflowData(MuleWorkflowApproval wfa){
+		LOGGER.info("approveWorkflowData");
+		final WorkflowApprovalMessage wam = serviceImpl.approveWorkflowData(wfa);
+		LOGGER.info("approveWorkflowData for workflowId {}", wam.getWorkflowId());
+		return new ResponseEntity<WorkflowApprovalMessage>(wam, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/report", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Report>> getWorkflowReport(
+			@RequestParam(value = "upc", required = true) final String upc, 
+			@RequestParam(value = "categories", required = true) final String categories,
+			@RequestParam(value = "vendorNo", required = true) final String vendorNo,
+			@RequestParam(value = "reasonCode", required = true) final String reasonCode,
+			@RequestParam(value = "workflowStatus", required = true) final String workflowStatus,
+			@RequestParam(value = "backendStatus", required = true) final String backendStatus,
+			@RequestParam(value = "ragStatus", required = true) final String ragStatus,
+			@RequestParam(value = "actionDateFrom", required = true) final String actionDateFrom,
+			@RequestParam(value = "actionDateTo", required = true) final String actionDateTo,
+			@RequestParam(value = "actionTimeFrom", required = true) final String actionTimeFrom, 
+			@RequestParam(value = "actionTimeTo", required = true) final String actionTimeTo,
+			@RequestParam(value = "roleCode", required = true) final String roleCode){
+		LOGGER.info("getWorkflowReport for [categories: {}]", categories);
+		final List<Report> reportList = serviceImpl.getWorkflowReport(upc,categories ,vendorNo ,reasonCode ,workflowStatus ,backendStatus ,ragStatus ,actionDateFrom ,actionDateTo ,actionTimeFrom ,actionTimeTo,roleCode);
+		LOGGER.info("/report found {}", categories, reportList.size());
+		return new ResponseEntity<List<Report>>(reportList, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/vendors", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Vendor>> getVendors(
+			@RequestParam(value = "account_grp", required = true) final String account_grp){
+		LOGGER.info("getVendors for [account_grp: {}]", account_grp);
+		final List<Vendor> vendorList = serviceImpl.getVendors(account_grp);
+		LOGGER.info("/vendors?account_grp={} found {}", account_grp, vendorList.size());
+		return new ResponseEntity<List<Vendor>>(vendorList, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/equipment", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Equipment>> getEquipment(){
+		LOGGER.info("getEquipment");
+		final List<Equipment> equipmentList = serviceImpl.getEquipment();
+		LOGGER.info("/equipment found {}", equipmentList.size());
+		return new ResponseEntity<List<Equipment>>(equipmentList, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/product/{articleNo}/vendor/{vendorNo}/equipment", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<EquipmentDetails>> getEquipmentDetails(
+			@PathVariable("articleNo") final String articleNo,
+			@PathVariable("vendorNo") final String vendorNo,
+			@RequestParam(value = "validFrom", required = true) final String validFrom, 
+			@RequestParam(value = "orderUnit", required = true) final String orderUnit){
+		LOGGER.info("getEquipmentDetails for[vendorNo: {}] and [articleNo : {}] and [orderUnit:{}] and [validFrom:{}]", vendorNo, articleNo, orderUnit, validFrom);
+		final List<EquipmentDetails> equipmentDetailsList = serviceImpl.getEquipmentDetails(articleNo,vendorNo,validFrom,orderUnit);
+		LOGGER.info("/product/{}/vendor/{}/equipment?validFrom={} found {}", articleNo,vendorNo,validFrom,equipmentDetailsList.size());
+		return new ResponseEntity<List<EquipmentDetails>>(equipmentDetailsList, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/product/{articleNo}/vendor/{vendorNo}/weight", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<ProductWeights>> getProductsWeights(
+			@PathVariable("articleNo") final String articleNo,
+			@PathVariable("vendorNo") final String vendorNo,
+			@RequestParam(value = "validFrom", required = true) final String validFrom){
+		LOGGER.info("getProductsWeights for[vendorNo: {}] and [articleNo : {}] and [validFrom:{}]", vendorNo, articleNo, validFrom);
+		final List<ProductWeights> productWeightsList = serviceImpl.getProductsWeights(articleNo,vendorNo,validFrom);
+		LOGGER.info("/product/{}/vendor/{}/weight?validFrom={} found {}", articleNo,vendorNo,validFrom,productWeightsList.size());
+		return new ResponseEntity<List<ProductWeights>>(productWeightsList, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/email", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public void sendEmail(
+			@RequestParam(value = "emailIds", required = true) final String emailIds,
+			@RequestParam(value = "emailBody", required = true) final String emailBody,
+			@RequestParam(value = "emailSubject", required = true) final String emailSubject){
+		LOGGER.info("sendEmail for [emailAddresses: {}]", emailIds);
+		final String status = serviceImpl.sendEmail(emailIds,emailBody,emailSubject);
+		LOGGER.info("/email {}" ,status);
+	}	
+	//addParamRoleForDualStageWorkflowRequest(roleCode)	
 }
--- RdaStub/src/main/java/com/mns/service/impl/RdaStubServiceImpl.java
+++ RdaStub/src/main/java/com/mns/service/impl/RdaStubServiceImpl.java
@@ -3,6 +3,8 @@ package com.mns.service.impl;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.List;
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,23 +16,40 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import com.mns.domain.Category;
+import com.mns.domain.Equipment;
+import com.mns.domain.EquipmentDetails;
+import com.mns.domain.MuleWorkflowApproval;
+import com.mns.domain.MuleWorkflowRequest;
 import com.mns.domain.Product;
+import com.mns.domain.ProductWeights;
+import com.mns.domain.Report;
+import com.mns.domain.SiteGroup;
+import com.mns.domain.SupplyChain;
 import com.mns.domain.Upt;
+import com.mns.domain.Vendor;
+import com.mns.domain.Workflow;
+import com.mns.domain.message.WorkflowApprovalMessage;
+import com.mns.domain.message.WorkflowCreationMessage;
 
 @Service
 public class RdaStubServiceImpl {
-private static final Logger LOGGER = LoggerFactory.getLogger(RdaStubServiceImpl.class.getName());
-	
-	public List<Product> fetchProducts(final String vendorNo, final String categories){
-		LOGGER.info("fetchProducts");
+	private static final Logger LOGGER = LoggerFactory.getLogger(RdaStubServiceImpl.class.getName());
+
+	public List<Product> getProducts(final String vendorNo, final String categories) {
+		LOGGER.info("getProducts");
 		Reader reader = null;
-		List<Product> productList = null; 
-		try{
-			reader = new FileReader(ResourceUtils.getFile("classpath:json\\product.json"));
-			TypeToken<List<Product>> token = new TypeToken<List<Product>>() {};
-			productList = new Gson().fromJson(reader, token.getType());
-			System.out.println(productList.size());
-		}catch(Exception e) {
+		List<Product> productList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\products.json"));
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(categories);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("F33");
+			}
+			TypeToken<List<Product>> token = new TypeToken<List<Product>>(){};
+			productList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return productList;
@@ -40,18 +59,17 @@ private static final Logger LOGGER = LoggerFactory.getLogger(RdaStubServiceImpl.
 		LOGGER.info("getUpt");
 		Reader reader = null;
 		List<Upt> uptList = null;
-		try{
+		try {
 			reader = new FileReader(ResourceUtils.getFile("classpath:json\\upt.json"));
 			JsonParser jsonParser = new JsonParser();
-            JsonObject jsonObj = (JsonObject)jsonParser.parse(reader);
-            JsonArray jsonArr = jsonObj.getAsJsonArray(articleNo);	
-            if(jsonArr==null) {
-            	jsonArr = jsonObj.getAsJsonArray("FOOD");
-            }
-			TypeToken<List<Upt>> token = new TypeToken<List<Upt>>() {};
-			uptList = new Gson().fromJson(reader, token.getType());
-			System.out.println(uptList.size());
-		}catch(Exception e) {
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(articleNo);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("000000000060051267");
+			}
+			TypeToken<List<Upt>> token = new TypeToken<List<Upt>>(){};
+			uptList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return uptList;
@@ -61,19 +79,217 @@ private static final Logger LOGGER = LoggerFactory.getLogger(RdaStubServiceImpl.
 		LOGGER.info("getCategories");
 		Reader reader = null;
 		List<Category> categoryList = null;
-		try{
+		try {
 			reader = new FileReader(ResourceUtils.getFile("classpath:json\\categories.json"));
 			JsonParser jsonParser = new JsonParser();
-            JsonObject jsonObj = (JsonObject)jsonParser.parse(reader);
-            JsonArray jsonArr = jsonObj.getAsJsonArray(grp_category);	
-            if(jsonArr==null) {
-            	jsonArr = jsonObj.getAsJsonArray("FOOD");
-            }
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(grp_category);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("FOOD");
+			}
 			TypeToken<List<Category>> token = new TypeToken<List<Category>>(){};
 			categoryList = new Gson().fromJson(jsonArr, token.getType());
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}			
+		}
 		return categoryList;
-	}	
+	}
+
+	public List<Product> getProduct(String articleNo, String categories) {
+		LOGGER.info("getProduct");
+		Reader reader = null;
+		List<Product> productList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\product.json"));
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(articleNo);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("000000000060010994");
+			}
+			TypeToken<List<Product>> token = new TypeToken<List<Product>>(){};
+			productList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return productList;
+	}
+
+	public List<SiteGroup> getSiteGroups(String classType) {
+		LOGGER.info("getSiteGroups");
+		Reader reader = null;
+		List<SiteGroup> SiteGroupList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\siteGroups.json"));
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(classType);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("ZFC");
+			}
+			TypeToken<List<SiteGroup>> token = new TypeToken<List<SiteGroup>>(){};
+			SiteGroupList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return SiteGroupList;
+	}
+
+	public List<SupplyChain> getSupplyChainData(String articleNo, String sitegroup, String classType,String validFrom) {
+		LOGGER.info("getSupplyChainData");
+		Reader reader = null;
+		List<SupplyChain> supplyChainList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\supplyChain.json"));
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(articleNo);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("000000000060028909");
+			}
+			TypeToken<List<SupplyChain>> token = new TypeToken<List<SupplyChain>>(){};
+			supplyChainList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return supplyChainList;
+	}
+
+	public WorkflowCreationMessage triggerWorkflowData(MuleWorkflowRequest wfr) {
+		LOGGER.info("triggerWorkflowData");
+		WorkflowCreationMessage wcm = new WorkflowCreationMessage();
+		wcm.setCreatedBy(wfr.getRequestor());
+		wcm.setWorkflowId("0000"+String.valueOf(new Random().nextInt(99999999)));
+		wcm.setWorkflowMessage("Workflow Created Successfully");
+		return wcm;
+	}
+
+	public List<Workflow> getPendingWorkflows(String categories) {
+		LOGGER.info("getPendingWorkflows");
+		Reader reader = null;
+		List<Workflow> workflowList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\pendingWorkflows.json"));
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(categories);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("F22");
+			}
+			TypeToken<List<Workflow>> token = new TypeToken<List<Workflow>>(){};
+			workflowList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return workflowList;
+	}
+
+	public WorkflowApprovalMessage approveWorkflowData(MuleWorkflowApproval wfa) {
+		LOGGER.info("approveWorkflowData");
+		WorkflowApprovalMessage wam = new WorkflowApprovalMessage();
+		wam.setWorkflowId(wfa.getWorkflowId());
+		wam.setWorkflowMessage("Successful Update");
+		wam.setWorkflowStatus("Closed");
+		return wam;
+	}
+
+	public List<Report> getWorkflowReport(String upc, String categories, String vendorNo, String reasonCode,
+			String workflowStatus, String backendStatus, String ragStatus, String actionDateFrom, String actionDateTo,
+			String actionTimeFrom, String actionTimeTo, String roleCode) {
+		LOGGER.info("getWorkflowReport");
+		Reader reader = null;
+		List<Report> reportList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\report.json"));
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(upc);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("117968");
+			}
+			TypeToken<List<Report>> token = new TypeToken<List<Report>>(){};
+			reportList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return reportList;
+	}
+
+	public List<Vendor> getVendors(String account_grp) {
+		LOGGER.info("getVendors");
+		Reader reader = null;
+		List<Vendor> VendorList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\vendors.json"));
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(account_grp);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("FOOD");
+			}
+			TypeToken<List<Vendor>> token = new TypeToken<List<Vendor>>(){};
+			VendorList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return VendorList;
+	}
+
+	public List<Equipment> getEquipment() {
+		LOGGER.info("getEquipment");
+		Reader reader = null;
+		List<Equipment> equipmentList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\equipment.json"));
+			TypeToken<List<Equipment>> token = new TypeToken<List<Equipment>>(){};
+			equipmentList = new Gson().fromJson(reader, token.getType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return equipmentList;
+	}
+
+	public List<EquipmentDetails> getEquipmentDetails(String articleNo, String vendorNo, String validFrom, String orderUnit) {
+		LOGGER.info("getEquipmentDetails");
+		Reader reader = null;
+		List<EquipmentDetails> equipmentDetailsList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\equipmentDetails.json"));
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(articleNo);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("000000000060121523");
+			}
+			TypeToken<List<EquipmentDetails>> token = new TypeToken<List<EquipmentDetails>>(){};
+			equipmentDetailsList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return equipmentDetailsList;
+	}
+
+	public List<ProductWeights> getProductsWeights(String articleNo, String vendorNo, String validFrom) {
+		LOGGER.info("getProductsWeights");
+		Reader reader = null;
+		List<ProductWeights> ProductWeightsList = null;
+		try {
+			reader = new FileReader(ResourceUtils.getFile("classpath:json\\productWeights.json"));
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObj = (JsonObject) jsonParser.parse(reader);
+			JsonArray jsonArr = jsonObj.getAsJsonArray(articleNo);
+			if (jsonArr == null) {
+				jsonArr = jsonObj.getAsJsonArray("000000000060121523");
+			}
+			TypeToken<List<ProductWeights>> token = new TypeToken<List<ProductWeights>>(){};
+			ProductWeightsList = new Gson().fromJson(jsonArr, token.getType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ProductWeightsList;
+	}
+
+	public String sendEmail(String emailIds, String emailBody, String emailSubject) {
+		return "Mail Sent!!";
+	}
 }
